9665bfd59ff304409c9ff46f1ddbb98b259a0e63
Chitranshi Yadav <yadavchitranshi89@gmail.com>
2026-05-19 00:41:51 +0530

    Added type casting

--- TypeCasting.py
+++ TypeCasting.py
@@ -211,16 +211,4 @@ print(type(s4))
 
 
 
-# input() function
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print(a + b)
-
-
-a = input("Enter first number: ")
-b = input("Enter second number: ")
-print(a + b)
-
-b1 = input("Enter name: ")
-print(b1)
